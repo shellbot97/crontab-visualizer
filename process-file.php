36fc6520d0d581347c26cd2051d1d81dc5d557f7
MEDIA\hitesh.i <hitesh.i@media.net>
2022-05-23 18:28:17 +0530

    removed current time

--- process-file.php
+++ process-file.php
@@ -8,6 +8,7 @@ $enclosure = '"'; //parameter for fputcsv
 
 $phpExecutionPath = (isset($_POST['executionPath']) && !empty($_POST['executionPath'])) ? $_POST['executionPath'] : '/usr/bin/php';
 $timezone = (isset($_POST['timezone']) && !empty($_POST['timezone'])) ? $_POST['timezone'] : "UTC";
+$sortByTime = (isset($_POST['sortByTime']) && !empty($_POST['sortByTime'])) ? $_POST['sortByTime'] : "0";
 $countOfNextOccurances = (isset($_POST['countOfNextOccurances']) && !empty($_POST['countOfNextOccurances'])) ? $_POST['countOfNextOccurances'] : "1";
 
 date_default_timezone_set($timezone);
@@ -25,32 +26,30 @@ while ($cronSchedule = fgets($cronTabPointer)) {
         continue;
     }
     $allCrons[] = [
-        'time' => \Cron\CronExpression::factory($cronInfo[0])->getNextRunDate($currentTime)->format('Y-m-d H:i:s'),
-        'expression' => CronSchedule::fromCronString(trim($cronInfo[0]))->asNaturalLanguage(),
-        'nextExecutionTimes' => implode("\n", array_map(function($iteration) use ($currentTime, $cronInfo){
+        'Cron Expression' => trim($cronInfo[0]),
+        'Parsed Expression' => CronSchedule::fromCronString(trim($cronInfo[0]))->asNaturalLanguage(),
+        'Next Execution Times' => implode("\n", array_map(function($iteration) use ($currentTime, $cronInfo){
             return \Cron\CronExpression::factory($cronInfo[0])->getNextRunDate($currentTime, $iteration)->format('Y-m-d H:i:s');
         }, range(1, $countOfNextOccurances))),
-        'filePath' => explode(" ", trim(explode('>>', $cronInfo[1])[0]))[0],
-        'arguments' => implode(" | ", array_slice(explode(" ", trim(explode('>>', $cronInfo[1])[0])), 1)),
-        'logPath' => explode('>>', $cronInfo[1])[1],
-        'previousRunDate' => \Cron\CronExpression::factory($cronInfo[0])->getPreviousRunDate($currentTime)->format('Y-m-d H:i:s'),
+        'File Path' => explode(" ", trim(explode('>>', $cronInfo[1])[0]))[0],
+        'Arguments' => implode(" | ", array_slice(explode(" ", trim(explode('>>', $cronInfo[1])[0])), 1)),
+        'Log Path' => explode('>>', $cronInfo[1])[1],
+        'Previous Run Date' => \Cron\CronExpression::factory($cronInfo[0])->getPreviousRunDate($currentTime)->format('Y-m-d H:i:s'),
+        'time' => \Cron\CronExpression::factory($cronInfo[0])->getNextRunDate($currentTime)->format('Y-m-d H:i:s'),
     ];
 }
 fclose($cronTabPointer);
-$allCrons[] = [
-    'time' => $currentTime,
-    'filePath' => 'current time',
-    'logPath' => 'current time',
-    "previousRunDate" => '',
-    'other' => '',
-];
-array_multisort(
-    array_map('strtotime', array_column($allCrons, 'time')), 
-    SORT_ASC, 
-    $allCrons
-);
+
+if ($sortByTime === '1') {
+    array_multisort(
+        array_map('strtotime', array_column($allCrons, 'time')), 
+        SORT_ASC, 
+        $allCrons
+    );
+}
 
 $file = fopen("crontab-$currentDate.csv", 'w');
+fputcsv($file, ['CURRENT TIME', $currentTime]);
 fputcsv($file, array_keys($allCrons[1]));
 foreach ($allCrons as $cron) {
     @fputcsv($file, $cron, $delimiter, $enclosure);
